3555404c9da44d6d9727d8d1c59bde3d16c355c7
HHOWI <minsu705@gmail.com>
2023-09-08 16:52:09 +0900

    오라클연결

---
+++ src/main/java/com/kh/elephant/ElephantApplication.java
@@ -0,0 +1,13 @@
+package com.kh.elephant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantApplication.class, args);
+	}
+
+}
