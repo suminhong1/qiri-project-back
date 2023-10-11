4b74716424741460c7acfbe68c99f85d9f382d30
mayerjogn <130232731+mayerjogn@users.noreply.github.com>
2023-10-11 19:26:03 +0900

    ㄴㅁㅇㅁㄴ

--- src/main/java/com/kh/elephant/security/WebSecurityConfig.java
+++ src/main/java/com/kh/elephant/security/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/qiri/userInfo/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/qiri/public/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/qiri/categoryType")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/qiri/post")).permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
